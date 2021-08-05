a9fbff974ac86a1120ab9ab1128dc81004515f3e
Kevin Wand <kevinwand@Kevins-Air.home>
2021-08-05 11:02:30 -0400

    kyu 5 & 6

--- cw_0805.js
+++ cw_0805.js
@@ -24,10 +24,10 @@ function rot13(message){
 // ("Grfg" == rot13("Test")
 // console.log(rot13("test"))
 // console.log(rot13("Test"))
-console.log(rot13("Codewars")) //Pbqrjnef
-console.log(rot13("abcdefghijklmnopqrstuvwxyz")) // nopqrstuvwxyzabcdefghijklm
-console.log(rot13("Test"))
-console.log(rot13("Test"))
+// console.log(rot13("Codewars")) //Pbqrjnef
+// console.log(rot13("abcdefghijklmnopqrstuvwxyz")) // nopqrstuvwxyzabcdefghijklm
+// console.log(rot13("Test"))
+// console.log(rot13("Test"))
 
 /*
 alternate solution
@@ -36,6 +36,36 @@ function rot13(message) {
   var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
   return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+}
+
+*/
+
+function songDecoder(song){
+  // const wub = song.replaceAll('WUB','')
+  const wub = /WUB{1,}?/g
+  const spaces = /  +/g
+  song = song.replace(wub, " ")
+  song = song.replace(spaces, " ")
+  return song.trim()
+}
+
+// (songDecoder("AWUBBWUBC"), "A B C","WUB should be replaced by 1 space");
+// (songDecoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C","multiples WUB should be replaced by only 1 space");
+// (songDecoder("WUBAWUBBWUBCWUB"), "A B C","heading or trailing spaces should be removed");
+console.log(songDecoder("AWUBBWUBC"))
+console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"))
+console.log(songDecoder("WUBAWUBBWUBCWUB"))
+
+
+/*
+alternate solutions
+
+function songDecoder(song){
+  return song.replace(/(WUB)+/g," ").trim()
+}
+
+function songDecoder(song){
+  return song.split('WUB').filter(Boolean).join(' ');
 }
 
 */
