3bb8f017197753130cf8fdc0ccc307315a6bbd18
Kevin Wand <kevinwand@Kevins-Air.home>
2021-08-20 13:17:10 -0400

    kyu 5

--- cw_0819.js
+++ cw_0819.js
@@ -74,42 +74,58 @@ const removeDuplicateIds = (obj) => {
   return answer
 };
 
-const obj = {
-  "1": ["A", "B", "C"],
-  "2": ["A", "B", "D", "A"],
-};
+// const obj = {
+//   "1": ["A", "B", "C"],
+//   "2": ["A", "B", "D", "A"],
+// };
 // const result = removeDuplicateIds(obj);
 // console.log(result)               // { "1": ["C"], "2": ["A", "B", "D"] });
 
 
-const obj1 = {
-  "1": ["C", "F", "G"],
-  "2": ["A", "B", "C"],
-  "3": ["A", "B", "D"],
-};
+// const obj1 = {
+//   "1": ["C", "F", "G"],
+//   "2": ["A", "B", "C"],
+//   "3": ["A", "B", "D"],
+// };
 // const result1 = removeDuplicateIds(obj1);
 // console.log(result1)              // { "1": ["F", "G"], "2": ["C"], "3": ["A", "B", "D"] });
 
 
-const obj2 = {
-  "1": ["A"],
-  "2": ["A"],
-  "3": ["A"],
-};
+// const obj2 = {
+//   "1": ["A"],
+//   "2": ["A"],
+//   "3": ["A"],
+// };
 // const result2 = removeDuplicateIds(obj2);
 // console.log(result2)              // { "1": [], "2": [], "3": ["A"] });
 
 
-const obj3 = {
-  "432": ["A", "A", "B", "D"],
-  "53": ["L", "G", "B", "C"],
-  "236": ["L", "A", "X", "G", "H", "X"],
-  "11": ["P", "R", "S", "D"],
-};
-const result3 = removeDuplicateIds(obj3);
-console.log(result3)              // {
+// const obj3 = {
+//   "432": ["A", "A", "B", "D"],
+//   "53": ["L", "G", "B", "C"],
+//   "236": ["L", "A", "X", "G", "H", "X"],
+//   "11": ["P", "R", "S", "D"],
+// };
+// const result3 = removeDuplicateIds(obj3);
+// console.log(result3)              // {
                                   // "11": ["P", "R", "S"],
                                   // "53": ["C"],
                                   // "236": ["L", "X", "G", "H"],
                                   // "432": ["A", "B", "D"],
                                   // });
+
+
+// function mergesorted(a, b) {
+//   a = a + b
+//   console.log(a)
+//   if ((a.length + b.length) === 1) {
+//     return (a)
+//   }
+//   // return a,b
+
+// }
+
+// mergesorted([1],[]) //should: [1,2,3,4]
+// mergesorted([1],[]) //should: [1,2,3,4]
+// mergesorted([1,2],[3]) //should: [1,2,3]
+// mergesorted([1],[2, 3]) //should: [1,2,3]
---
+++ cw_0820.js
@@ -0,0 +1,53 @@
+var numberToPrice = function(number) {
+  if (typeof number !== "number") { return 'NaN' }
+  number = number.toString()
+  let decimal = number.substring(number.indexOf(".") + 1)
+  let beforeZero = number.length
+  
+  // solves for after decimal issues
+  if (number.includes(".")) {
+    let afterZero = number.substring(number.indexOf(".") + 1).length
+    console.log("afterZero",afterZero)
+    beforeZero = number.length - (afterZero + 1)
+    console.log("beforeZero",beforeZero)
+    if (afterZero === 2) {
+      decimal = decimal
+    } else if (afterZero > 2) {
+      decimal = decimal.substring(0,2) + "." + decimal.substring(2)
+      decimal = Math.round(parseInt(decimal)).toString()
+    } else {
+      decimal = decimal + "0"
+    }
+    number = number.substring(0,beforeZero) + "." + decimal
+  } else {
+    number += ".00"
+  }
+
+  console.log(beforeZero)
+  console.log(number)
+
+  // solve for before decimal
+  if (number.includes("-")) {
+    number = number.substring(1)
+    beforeZero = beforeZero - 1
+    while (beforeZero > 3) {
+      number = number.substring(0, beforeZero - 3) + "," + number.substring(beforeZero - 3)
+      beforeZero = beforeZero - 3
+    }
+    number = "-"+number
+  } else {
+    while (beforeZero > 3) {
+      number = number.substring(0, beforeZero - 3) + "," + number.substring(beforeZero - 3)
+      beforeZero = beforeZero - 3
+    }
+  }
+
+  return (number)
+}
+
+console.log(numberToPrice(245123215.00))        //'245,213,215.00'
+console.log(numberToPrice(1500.129))            //'1,500.12'
+console.log(numberToPrice(-5))                  //'-5.00'
+console.log(numberToPrice(1000000.5))           //'1,000,000.50'
+console.log(numberToPrice('@'))                 //'NaN'
+
